43f52c087547e987ad6c9e9951a8abde6fb1b5fb
ivansaul <saul_2503@hotmail.com>
2024-12-02 22:23:19 -0500

    chore: add SwiftUI playground

---
+++ docs/assets/code/swiftui/SwiftUIPG/SwiftUIPG/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIPG
+//
+//  Created by @ivansaul on 12/1/24.
+//
+//  https://github.com/ivansaul
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
